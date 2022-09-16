9a6540c9122585e2de1a5c35090b8fd89d67b24b
jieLi <2603041199@qq,com>
2022-09-16 17:46:00 +0800

    init

---
+++ src/main/java/com/peixun/test/yourfirst/YourfirstApplication.java
@@ -0,0 +1,13 @@
+package com.peixun.test.yourfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourfirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourfirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peixun/test/yourfirst/YourfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peixun.test.yourfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourfirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
